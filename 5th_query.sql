e4c2e85592d39d68454e720718ac5786edcca272
katjas <katarzyna.dusinska92@gmail.com>
2019-08-07 20:42:19 +0200

    message

--- 5th_query.sql
+++ 5th_query.sql
@@ -13,3 +13,32 @@ join public."Employee" as e on
 join public."Customer" as c2 on
 	c2."SupportRepId"=e."EmployeeId"
 where c."CustomerId"=5;
+
+-- II rozwiązanie
+
+with base_customer as (
+select
+	c."SupportRepId" as empl_id
+from
+	public."Customer" as c
+where
+	c."CustomerId" = 5)
+select bc.empl_id,c."FirstName", c."LastName" from base_customer as bc join public."Customer" as c on c."SupportRepId" = bc.empl_id;
+
+-- III rozwiązanie
+
+select
+	c."FirstName",
+	c."LastName",
+	e."EmployeeId"
+from
+	public."Customer" as c
+join public."Employee" as e on
+	c."SupportRepId" = e."EmployeeId"
+where
+	e."EmployeeId" = (
+		select c2."SupportRepId"
+	from
+		public."Customer" as c2
+	where
+		c2."CustomerId" = 5);
