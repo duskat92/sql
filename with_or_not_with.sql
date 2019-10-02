7fca9f89a60a80622b333ad5d7cfd2be2050777d
katjas <katarzyna.dusinska92@gmail.com>
2019-10-02 20:38:40 +0200

    message

--- with_or_not_with.sql
+++ with_or_not_with.sql
@@ -19,3 +19,27 @@ with task as (select c.category_id
 		c.category_name = 'Produce')
 select * from task join eshop.products as p on
 p.category_id=task.category_id;
+
+
+select * from eshop.orders as o
+where
+o.order_id%2 = 0;
+
+
+-- and nieparzyste
+
+select * from eshop.orders as o
+where
+o.order_id%2 = 1;
+
+--and podzielne przez 10
+
+select * from eshop.orders as o
+where
+o.order_id%10 != 0;
+
+--and niepodzielne przez 10
+
+select * from eshop.orders as o
+where
+not o.order_id%10 = 0;
